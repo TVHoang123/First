e12853cd9a5b113c348b4e6d038e930fd666757d
HoangGk <thaihoang2003vvv@gmail.com>
2022-08-02 13:11:05 +0700

    nap lan dau

---
+++ abcd.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#define bit(n, i) ((n>>i)&1)
+const int mod = 1e9+7;
+typedef long long ll;
+ 
+int main() {
+	int t;cin >> t;
+	while(t--) {
+		
+	}
+}
