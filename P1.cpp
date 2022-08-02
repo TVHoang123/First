43ffecc3ed5758f1823458ae27803ca98619bb0c
HoangGk <thaihoang2003vvv@gmail.com>
2022-08-02 13:40:08 +0700

    He

---
+++ P1.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+const int mod = 1e9+7;
+typedef long long ll;
+int main() {
+    
+}
